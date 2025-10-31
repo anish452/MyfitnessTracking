12bfe37ff71ffbe9566593031398e41893355d83
Hasitha Wickramage <hasithawickramage@gmail.com>
2025-10-31 13:20:17 +1300

    create api for create activities daily config serializers.py urls and views.py

--- api/serializers.py
+++ api/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from .models import Activity
 from django.contrib.auth.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
 class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
         fields = '__all__'
         read_only_fields = ('id', 'user', 'created_at')
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email')
