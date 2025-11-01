d00aed02278e0177a4cef0edf4dffb9a06032bce
Hasitha Wickramage <hasithawickramage@gmail.com>
2025-11-02 12:18:21 +1300

    gunicorn fitness_project.wsgi:application in render startup

--- fitness_project/urls.py
+++ fitness_project/urls.py
@@ -24,6 +24,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    # path('api/token/', TokenObtainPairView.as_view(), name='token'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
 ]
